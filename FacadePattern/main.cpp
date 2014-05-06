518ffef5399be0ac977674bc1e07c26ae59e1c4a
lhoussaine <lhoussaine@localhost>
2014-05-06 10:18:37 +0100

    Merge origin/master into FactoryPattern
    Conflicts:
    	README.md

---
+++ FacadePattern/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: lhoussaine
+ *
+ * Created on 6 mai 2014, 10:18
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
