b914a6450d604c4a665c3b0471998d83edd25f04
lhoussaine <lhoussaine@localhost>
2014-05-06 08:40:18 +0100

    This is an example of using Design Pattern Factory, i tried to make it simple as possible, you can check it and use it as you want in your future project.

---
+++ SingletonPattern/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: lhoussaine
+ *
+ * Created on 5 mai 2014, 16:41
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
